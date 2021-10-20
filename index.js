2ab23a68937e8c04fa381f4bc93932f6ac11d1a4
PurviMec <princy.mecwan94@gmail.com>
2021-10-19 20:44:15 -0400

    commit

--- index.js
+++ index.js
@@ -5,124 +5,112 @@ const inquirer = require('inquirer');
 
 const generatePage = require('./src/page-template');
 
-const userInfo = () =>{
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Write your Project Name here:(Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your name!');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide some information about your project (like overview of how it works, technologies used, future implements and challenges):(Required)'
-        },
-        {
-            type: 'list',
-            name: 'Table of Contents',
-            message: '',
-            choices: ['Installation', 'Usage', 'Contributing', 'License']
-        }
-      
-    ]);
-};
 
-const mainData = READMEdata => {
-    if (!READMEdata.projects) {
-        READMEdata.projects = [];
-    }
+
+const mainData = () => {
+   
     return inquirer
         .prompt([
-        {
-            type: 'input',
-            name: 'Link',
-            message: 'Link to deployed application:(Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter link!');
-                  return false;
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Write your Project Name here:(Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                      return true;
+                    } else {
+                      console.log('Please enter your name!');
+                      return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'Please guide us step-by-step description of how to get the development environment running.'
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'Provide instructions and examples for use'
-        },
-        {
-            type: 'fuzzypath',
-            name: 'path',
-            excludePath: nodePath => nodePath.startsWith('node_modules'),
-            excludeFilter: nodePath => nodePath == '.',
-            itemType: 'any',
-            rootPath: 'app',
-            message: 'Select a target directory for your image:(using the relative filepath, add it to your README using the following syntax:)',
-            default: '![alt text](assets/images/screenshot.png)',
-            suggestOnly: false,
-            depthLimit: 5
-        },
-        {
-            type: 'input',
-            name: 'Contributing',
-            message: 'Add guidelines for cintribution:'
-        },
-        {
-            type: 'checkbox',
-            name: 'license',
-            message: 'What type of licence you have?',
-            choices: ['BSD', 'MIT', 'GPL']
-        },
-        {
-            type: 'input',
-            name: 'Tests',
-            message: 'Add your tests and examples how to run them:'
-        },
-        {
-            type: 'input',
-            name: 'GitHub',
-            message: 'What is your GitHub user-name?',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your user-name!');
-                  return false;
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide some information about your project (like overview of how it works, technologies used, future implements and challenges):(Required)'
+            },
+            {
+                type: 'input',
+                name: 'link',
+                message: 'Link to deployed application:(Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter link!');
+                        return false;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'Email',
-            message: 'What is your Email address?',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your Email!');
-                  return false;
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Please guide us step-by-step description of how to get the development environment running.'
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Provide instructions and examples for use'
+            },
+            {
+                type: 'fuzzypath',
+                name: 'path',
+                excludePath: nodePath => nodePath.startsWith('node_modules'),
+                excludeFilter: nodePath => nodePath == '.',
+                itemType: 'any',
+                rootPath: 'app',
+                message: 'Select a target directory for your image:(using the relative filepath, add it to your README using the following syntax:)',
+                default: '![alt text](assets/images/screenshot.png)',
+                suggestOnly: false,
+                depthLimit: 5
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'Add guidelines for cintribution:'
+            },
+            {
+                type: 'checkbox',
+                name: 'license',
+                message: 'What type of licence you have?',
+                choices: ['BSD', 'MIT', 'GPL']
+            },
+            {
+               type: 'input',
+                name: 'tests',
+                message: 'Add your tests and examples how to run them:'
+            },
+            {
+                type: 'input',
+                name: 'gitHub',
+                message: 'What is your GitHub user-name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your user-name!');
+                        return false;
+                    }
                 }
-            }
-        },
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Email address?',
+                validate: nameInput => {
+                    if (nameInput) {
+                       return true;
+                    } else {
+                       console.log('Please enter your Email!');
+                       return false;
+                    }
+                }
+            },
     ]);
 }
 
-userInfo()
-.then(mainData)
+
+mainData()
 .then(READMEdata => {
     const pageHTML = generatePage(READMEdata); 
     fs.writeFile('./index.html', pageHTML, err =>{
--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,7 @@
 
 module.exports = templateData => {
-    const { main, ...header } = templateData;
+  console.log(templateData);
+  const { about, ...header } = templateData;
     return`
     <!DOCTYPE html> 
   <html lang="en"> 
@@ -13,12 +14,60 @@ module.exports = templateData => {
 
   <body>
       <header>
-          <h1>${header.name}</h1>
-          <p><h3>Discription</h3>
-              ${header.description}
-          </p>
+          <div>
+              <div>
+                  <h1>${header.name}</h1>
+                  <p><h3>Discription</h3>
+                      ${header.description}
+                  </p>
+              </div>
+              <div>
+                  ${header.link}
+              </div>
+          <div>
+              <p>
+                  <h3>Table of Contents:</h3>
+                  <link href="" />Installation</br>
+                  <link href="" />Usage</br>
+                  <link href="" />Contributing</br>
+                  <link href="" />license</br>
+              </p>
+          </div>
       </header>
       <main>
+          <div>
+              <div>
+                  <p><h3>Installation</h3>
+                      ${header.installation}
+                  </p>
+              </div>
+              <div>
+                  <p><h3>Usage</h3>
+                      ${header.usage}
+                      <p>Select a target directory for your image:(using the relative filepath, add it to your README using the following syntax:</P>
+                      <span>${header.path}</span>
+                  </p>
+              </div>
+              <div>
+                  <p><h3>Contributing</h3>
+                      ${header.contributing}
+                  </p>
+              </div>
+              <div>
+                  <p><h3>license</h3>
+                      ${header.license}
+                  </p>
+              </div>
+              <div>
+                  <p><h3>Tests</h3>
+                      ${header.tests}
+                  </p>
+              </div>
+          </div>
+          <div>
+              <p>GitHub UserName: <a href="">${header.gitHub}</a></p>
+              <p>Email Address: <a href="">${header.email}</a></p>
+          </div>
       </main>
   </body>
   </html>
